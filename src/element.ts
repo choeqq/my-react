ec3c35ea1f942e55f6f5a992b2d7fbe2440bc4b5
choeqq <k.chola@op.pl>
2022-05-27 17:36:52 +0200

    createTextElement added

--- src/element.ts
+++ src/element.ts
@@ -6,6 +6,10 @@ export function createElement(type, config, ...args) {
   const rawChildren = hasChildren ? [].concat(...args) : [];
   props.children = rawChildren
     .filter((c) => c !== null && c !== false)
-    .map((c) => (c instanceof Obejct ? c : createElement(c)));
+    .map((c) => (c instanceof Obejct ? c : createTextElement(c)));
   return { type, props };
+}
+
+function createTextElement(value) {
+  return createElement(TEXT_ELEMENT, { nodeValue: value });
 }
