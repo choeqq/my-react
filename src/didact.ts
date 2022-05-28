86b699ca30514694a3fa3041e09b12917794e45f
choeqq <k.chola@op.pl>
2022-05-28 21:15:44 +0200

    reconcileCHildren added

--- src/didact.ts
+++ src/didact.ts
@@ -19,7 +19,25 @@ export function reconcile(parentDom, instance, element) {
     const newInstance = instantiate(element);
     parentDom.replaceChild(newInstance.dom, instance.dom);
     return newInstance;
+  } else if (typeof element.type === "string") {
+    updateDomProperties(instance.dom, instance.element.props, element.props);
+    instance.childInstances = reconcileChildren(instance, element);
   }
+}
+
+function reconcileChildren(instance, element) {
+  const dom = instance.dom;
+  const childInstances = instance.childInstances;
+  const nextChildElements = element.props.children || [];
+  const newChildInstances = [];
+  const count = Math.max(childInstances.length, nextChildElements.length);
+  for (let i = 0; i < count; i++) {
+    const childInstance = childInstances[i];
+    const childElement = nextChildElements[i];
+    const newChildInstance = reconcile(dom, childInstance, childElement);
+    newChildInstances.push(newChildInstance);
+  }
+  return newChildInstances.filter((instance) => instance != null);
 }
 
 function instantiate(element) {
