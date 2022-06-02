903c81887ddc29e310e43b3bca9b4415a991e217
choeqq <k.chola@op.pl>
2022-06-02 20:04:02 +0200

    render jsx dom tests completed

--- test/01.render-jsx-dom-elements.test.js
+++ test/01.render-jsx-dom-elements.test.js
@@ -14,3 +14,36 @@ test.beforeEach((t) => {
   }
   t.context.root = root;
 });
+
+test("render jsx div", (t) => {
+  const root = t.context.root;
+  const element = <div />;
+  render(element, root);
+  t.is(root.innerHTML, "<div></div>");
+});
+
+test("render div with children", (t) => {
+  const root = t.context.root;
+  const element = (
+    <div>
+      <b />
+      <a href="foo" />
+    </div>
+  );
+  render(element, root);
+  t.is(root.innerHTML, '<div><b></b><a href="foo"></a></div>');
+});
+
+test("render div with props", (t) => {
+  const root = t.context.root;
+  const element = <div id="foo" />;
+  render(element, root);
+  t.is(root.innerHTML, '<div id="foo"></div>');
+});
+
+test("render span with text child", (t) => {
+  const root = t.context.root;
+  const element = <span>Foo</span>;
+  render(element, root);
+  t.is(root.innerHTML, "<span>Foo</span>");
+});
